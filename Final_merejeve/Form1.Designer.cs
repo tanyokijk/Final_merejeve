3ea3a0fa2af7cdcbbf03f6224541dc4d454831e1
Тетяна Деревянко <t.derevyanko1209@gmail.com>
2023-11-29 22:34:38 +0200

    Добавьте файлы проекта.

---
+++ Final_merejeve/Form1.Designer.cs
@@ -0,0 +1,311 @@
+﻿namespace Final_merejeve
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.txtURL = new System.Windows.Forms.TextBox();
+            this.btnDownload = new System.Windows.Forms.Button();
+            this.btnCancel = new System.Windows.Forms.Button();
+            this.listBoxFiles = new System.Windows.Forms.ListBox();
+            this.btnDelete = new System.Windows.Forms.Button();
+            this.btnRename = new System.Windows.Forms.Button();
+            this.textBoxNewName = new System.Windows.Forms.TextBox();
+            this.btnMoveFile = new System.Windows.Forms.Button();
+            this.listBoxFailedDownloads = new System.Windows.Forms.ListBox();
+            this.listBoxCurrentlyDownloading = new System.Windows.Forms.ListBox();
+            this.numericUpDownThreads = new System.Windows.Forms.NumericUpDown();
+            this.btnHelp = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDownThreads)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // txtURL
+            // 
+            this.txtURL.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.txtURL.Font = new System.Drawing.Font("Microsoft Himalaya", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtURL.Location = new System.Drawing.Point(57, 86);
+            this.txtURL.Name = "txtURL";
+            this.txtURL.Size = new System.Drawing.Size(818, 19);
+            this.txtURL.TabIndex = 0;
+            // 
+            // btnDownload
+            // 
+            this.btnDownload.BackColor = System.Drawing.Color.White;
+            this.btnDownload.Image = ((System.Drawing.Image)(resources.GetObject("btnDownload.Image")));
+            this.btnDownload.Location = new System.Drawing.Point(1085, 34);
+            this.btnDownload.Name = "btnDownload";
+            this.btnDownload.Size = new System.Drawing.Size(83, 79);
+            this.btnDownload.TabIndex = 1;
+            this.btnDownload.UseVisualStyleBackColor = false;
+            this.btnDownload.Click += new System.EventHandler(this.btnDownload_Click);
+            // 
+            // btnCancel
+            // 
+            this.btnCancel.BackColor = System.Drawing.Color.White;
+            this.btnCancel.Image = ((System.Drawing.Image)(resources.GetObject("btnCancel.Image")));
+            this.btnCancel.Location = new System.Drawing.Point(1204, 34);
+            this.btnCancel.Name = "btnCancel";
+            this.btnCancel.Size = new System.Drawing.Size(83, 79);
+            this.btnCancel.TabIndex = 3;
+            this.btnCancel.UseVisualStyleBackColor = false;
+            this.btnCancel.Click += new System.EventHandler(this.btnCancel_Click);
+            // 
+            // listBoxFiles
+            // 
+            this.listBoxFiles.Font = new System.Drawing.Font("Microsoft Himalaya", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.listBoxFiles.FormattingEnabled = true;
+            this.listBoxFiles.ItemHeight = 19;
+            this.listBoxFiles.Location = new System.Drawing.Point(57, 236);
+            this.listBoxFiles.Name = "listBoxFiles";
+            this.listBoxFiles.Size = new System.Drawing.Size(302, 194);
+            this.listBoxFiles.TabIndex = 4;
+            // 
+            // btnDelete
+            // 
+            this.btnDelete.BackColor = System.Drawing.Color.White;
+            this.btnDelete.Image = ((System.Drawing.Image)(resources.GetObject("btnDelete.Image")));
+            this.btnDelete.Location = new System.Drawing.Point(609, 488);
+            this.btnDelete.Name = "btnDelete";
+            this.btnDelete.Size = new System.Drawing.Size(81, 79);
+            this.btnDelete.TabIndex = 5;
+            this.btnDelete.UseVisualStyleBackColor = false;
+            this.btnDelete.Click += new System.EventHandler(this.btnDelete_Click);
+            // 
+            // btnRename
+            // 
+            this.btnRename.BackColor = System.Drawing.Color.White;
+            this.btnRename.Image = ((System.Drawing.Image)(resources.GetObject("btnRename.Image")));
+            this.btnRename.Location = new System.Drawing.Point(407, 488);
+            this.btnRename.Name = "btnRename";
+            this.btnRename.Size = new System.Drawing.Size(81, 79);
+            this.btnRename.TabIndex = 6;
+            this.btnRename.UseVisualStyleBackColor = false;
+            this.btnRename.Click += new System.EventHandler(this.btnRename_Click);
+            // 
+            // textBoxNewName
+            // 
+            this.textBoxNewName.Font = new System.Drawing.Font("Microsoft Himalaya", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBoxNewName.Location = new System.Drawing.Point(57, 536);
+            this.textBoxNewName.Name = "textBoxNewName";
+            this.textBoxNewName.Size = new System.Drawing.Size(302, 31);
+            this.textBoxNewName.TabIndex = 7;
+            // 
+            // btnMoveFile
+            // 
+            this.btnMoveFile.BackColor = System.Drawing.Color.White;
+            this.btnMoveFile.Image = ((System.Drawing.Image)(resources.GetObject("btnMoveFile.Image")));
+            this.btnMoveFile.Location = new System.Drawing.Point(810, 488);
+            this.btnMoveFile.Name = "btnMoveFile";
+            this.btnMoveFile.Size = new System.Drawing.Size(81, 79);
+            this.btnMoveFile.TabIndex = 8;
+            this.btnMoveFile.UseVisualStyleBackColor = false;
+            this.btnMoveFile.Click += new System.EventHandler(this.btnMoveFile_Click);
+            // 
+            // listBoxFailedDownloads
+            // 
+            this.listBoxFailedDownloads.Font = new System.Drawing.Font("Microsoft Himalaya", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.listBoxFailedDownloads.FormattingEnabled = true;
+            this.listBoxFailedDownloads.ItemHeight = 19;
+            this.listBoxFailedDownloads.Location = new System.Drawing.Point(949, 236);
+            this.listBoxFailedDownloads.Name = "listBoxFailedDownloads";
+            this.listBoxFailedDownloads.Size = new System.Drawing.Size(302, 194);
+            this.listBoxFailedDownloads.TabIndex = 9;
+            // 
+            // listBoxCurrentlyDownloading
+            // 
+            this.listBoxCurrentlyDownloading.Font = new System.Drawing.Font("Microsoft Himalaya", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.listBoxCurrentlyDownloading.FormattingEnabled = true;
+            this.listBoxCurrentlyDownloading.ItemHeight = 19;
+            this.listBoxCurrentlyDownloading.Location = new System.Drawing.Point(514, 236);
+            this.listBoxCurrentlyDownloading.Name = "listBoxCurrentlyDownloading";
+            this.listBoxCurrentlyDownloading.Size = new System.Drawing.Size(302, 194);
+            this.listBoxCurrentlyDownloading.TabIndex = 10;
+            // 
+            // numericUpDownThreads
+            // 
+            this.numericUpDownThreads.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.numericUpDownThreads.Font = new System.Drawing.Font("Microsoft Himalaya", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.numericUpDownThreads.Location = new System.Drawing.Point(939, 83);
+            this.numericUpDownThreads.Maximum = new decimal(new int[] {
+            3,
+            0,
+            0,
+            0});
+            this.numericUpDownThreads.Minimum = new decimal(new int[] {
+            1,
+            0,
+            0,
+            0});
+            this.numericUpDownThreads.Name = "numericUpDownThreads";
+            this.numericUpDownThreads.Size = new System.Drawing.Size(54, 30);
+            this.numericUpDownThreads.TabIndex = 11;
+            this.numericUpDownThreads.Value = new decimal(new int[] {
+            1,
+            0,
+            0,
+            0});
+            // 
+            // btnHelp
+            // 
+            this.btnHelp.Image = ((System.Drawing.Image)(resources.GetObject("btnHelp.Image")));
+            this.btnHelp.Location = new System.Drawing.Point(1215, 562);
+            this.btnHelp.Name = "btnHelp";
+            this.btnHelp.Size = new System.Drawing.Size(81, 79);
+            this.btnHelp.TabIndex = 12;
+            this.btnHelp.UseVisualStyleBackColor = true;
+            this.btnHelp.Click += new System.EventHandler(this.btnHelp_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.BackColor = System.Drawing.Color.White;
+            this.label1.Font = new System.Drawing.Font("Microsoft Himalaya", 20.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(52, 34);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(562, 27);
+            this.label1.TabIndex = 13;
+            this.label1.Text = "Введіть URL-адресу файлу, який хочете завантажити";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Himalaya", 20.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(873, 34);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(187, 27);
+            this.label2.TabIndex = 14;
+            this.label2.Text = "Кількість потоків";
+            this.label2.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Himalaya", 27.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(1043, 196);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(125, 37);
+            this.label3.TabIndex = 15;
+            this.label3.Text = "Невдалі\r\n";
+            this.label3.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Himalaya", 27.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(602, 196);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(121, 37);
+            this.label4.TabIndex = 16;
+            this.label4.Text = "Поточні\r\n";
+            this.label4.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Microsoft Himalaya", 27.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.Location = new System.Drawing.Point(156, 196);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(92, 37);
+            this.label5.TabIndex = 17;
+            this.label5.Text = "Вдалі";
+            this.label5.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Font = new System.Drawing.Font("Microsoft Himalaya", 27.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label6.Location = new System.Drawing.Point(50, 496);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(263, 37);
+            this.label6.TabIndex = 18;
+            this.label6.Text = "Нова назва файлу";
+            this.label6.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("$this.BackgroundImage")));
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.ClientSize = new System.Drawing.Size(1308, 653);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.btnHelp);
+            this.Controls.Add(this.numericUpDownThreads);
+            this.Controls.Add(this.listBoxCurrentlyDownloading);
+            this.Controls.Add(this.listBoxFailedDownloads);
+            this.Controls.Add(this.btnMoveFile);
+            this.Controls.Add(this.textBoxNewName);
+            this.Controls.Add(this.btnRename);
+            this.Controls.Add(this.btnDelete);
+            this.Controls.Add(this.listBoxFiles);
+            this.Controls.Add(this.btnCancel);
+            this.Controls.Add(this.btnDownload);
+            this.Controls.Add(this.txtURL);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDownThreads)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txtURL;
+        private System.Windows.Forms.Button btnDownload;
+        private System.Windows.Forms.Button btnCancel;
+        private System.Windows.Forms.ListBox listBoxFiles;
+        private System.Windows.Forms.Button btnDelete;
+        private System.Windows.Forms.Button btnRename;
+        private System.Windows.Forms.TextBox textBoxNewName;
+        private System.Windows.Forms.Button btnMoveFile;
+        private System.Windows.Forms.ListBox listBoxFailedDownloads;
+        private System.Windows.Forms.ListBox listBoxCurrentlyDownloading;
+        private System.Windows.Forms.NumericUpDown numericUpDownThreads;
+        private System.Windows.Forms.Button btnHelp;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+    }
+}
+
---
+++ Final_merejeve/Form1.cs
@@ -0,0 +1,257 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement;
+
+namespace Final_merejeve
+{
+    public partial class Form1 : Form
+    {
+        WebClient webClient;
+        string filePathToSave=null;
+
+        public Form1()
+        {
+            InitializeComponent();
+            webClient = new WebClient();
+            webClient.DownloadFileCompleted += new AsyncCompletedEventHandler(DownloadCompleted);
+            label1.BackColor = Color.Transparent;
+            label2.BackColor = Color.Transparent;
+            label4.BackColor = Color.Transparent;
+            label3.BackColor = Color.Transparent;
+            label5.BackColor = Color.Transparent;
+            label6.BackColor = Color.Transparent;
+        }
+
+        private async void btnDownload_Click(object sender, EventArgs e)
+        {
+            string url = txtURL.Text;
+            SaveFileDialog saveFileDialog = new SaveFileDialog();
+
+            try
+            {
+                Uri uri = new Uri(url);
+                string extension = Path.GetExtension(uri.AbsolutePath);
+
+                if (string.IsNullOrEmpty(extension)) 
+                {
+                    throw new ArgumentException("Розширення файлу в URL не знайдено");
+                }
+
+                saveFileDialog.Filter = $"Files (*{extension})|*{extension}";
+                saveFileDialog.FileName = "downloaded_file" + extension;
+
+                if (saveFileDialog.ShowDialog() == DialogResult.OK)
+                {
+                    filePathToSave = saveFileDialog.FileName; 
+
+                    listBoxCurrentlyDownloading.Items.Add(filePathToSave);
+
+                    int threadsCount = (int)numericUpDownThreads.Value;
+
+                    var tasks = new List<Task>();
+
+                    for (int i = 0; i < threadsCount; i++)
+                    {
+                        tasks.Add(DownloadFileAsync(url, filePathToSave));
+                    }
+
+                    await Task.WhenAll(tasks);
+
+                    listBoxFiles.Items.Add(filePathToSave);
+                    listBoxCurrentlyDownloading.Items.Remove(filePathToSave); 
+
+                    MessageBox.Show("Файл успішно завантажено!");
+                }
+            }
+            catch (UriFormatException ex)
+            {
+                MessageBox.Show($"Помилка в URL: {ex.Message}");
+                listBoxFailedDownloads.Items.Add(filePathToSave);
+                listBoxCurrentlyDownloading.Items.Remove(filePathToSave);
+            }
+            catch (ArgumentException ex)
+            {
+                MessageBox.Show($"Помилка: {ex.Message}");
+                if (filePathToSave != null)
+                {
+                    listBoxFailedDownloads.Items.Add(filePathToSave);
+                    listBoxCurrentlyDownloading.Items.Remove(filePathToSave);
+                }
+
+            }
+            catch (PathTooLongException ex)
+            {
+                MessageBox.Show($"Довгий шлях файлу: {ex.Message}");
+                if (filePathToSave != null)
+                {
+                    listBoxFailedDownloads.Items.Add(filePathToSave);
+                    listBoxCurrentlyDownloading.Items.Remove(filePathToSave);
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show($"Виникла помилка: {ex.Message}");
+                if (filePathToSave != null)
+                {
+                    listBoxFailedDownloads.Items.Add(filePathToSave);
+                    listBoxCurrentlyDownloading.Items.Remove(filePathToSave);
+                }
+            }
+
+        }
+
+        private async Task DownloadFileAsync(string url, string filePathToSave)
+        {
+            using (var httpClient = new HttpClient())
+            {
+                var response = await httpClient.GetAsync(url);
+
+                if (response.IsSuccessStatusCode)
+                {
+                    using (var fileStream = File.Create(filePathToSave))
+                    {
+                        await response.Content.CopyToAsync(fileStream);
+                    }
+                }
+                else
+                {
+                    MessageBox.Show($"Не вдалося завантажити файл. Код: {response.StatusCode}");
+                }
+            }
+        }
+
+
+
+        private void DownloadCompleted(object sender, AsyncCompletedEventArgs e)
+        {
+            string fileName = listBoxCurrentlyDownloading.Items.Cast<string>().LastOrDefault();
+
+            if (e.Error != null)
+            {
+                MessageBox.Show("Помилка завантаження: " + e.Error.Message);
+
+                if (fileName != null)
+                {
+                    listBoxFailedDownloads.Items.Add(fileName);
+                }
+            }
+            else
+            {
+                MessageBox.Show("Файл успішно завантажено!");
+
+                if (fileName != null)
+                {
+                    listBoxFiles.Items.Add(fileName);
+                }
+            }
+            if (fileName != null)
+            {
+                listBoxCurrentlyDownloading.Items.Remove(fileName);
+            }
+        }
+
+        private void btnCancel_Click(object sender, EventArgs e)
+        {
+            if (webClient.IsBusy)
+            {
+                webClient.CancelAsync();
+            }
+        }
+
+        private void btnDelete_Click(object sender, EventArgs e)
+        {
+            if (listBoxFiles.SelectedIndex != -1)
+            {
+                string selectedFilePath = listBoxFiles.SelectedItem.ToString();
+                if (File.Exists(selectedFilePath))
+                {
+                    File.Delete(selectedFilePath); 
+                    listBoxFiles.Items.Remove(selectedFilePath);
+                }
+                else
+                {
+                    MessageBox.Show("Файл не знайдений.");
+                }
+            }
+
+        }
+
+        private void btnRename_Click(object sender, EventArgs e)
+        {
+            if (listBoxFiles.SelectedIndex != -1)
+            {
+                string selectedFilePath = listBoxFiles.SelectedItem.ToString();
+                string directoryPath = Path.GetDirectoryName(selectedFilePath);
+
+                string extension = Path.GetExtension(selectedFilePath); 
+
+                string newFileName = textBoxNewName.Text.Trim();
+                if (!string.IsNullOrWhiteSpace(newFileName))
+                {
+                    string fileNameWithoutExtension = Path.GetFileNameWithoutExtension(selectedFilePath); 
+                    string newFilePath = Path.Combine(directoryPath, newFileName + extension); 
+
+                    if (File.Exists(selectedFilePath))
+                    {
+                        File.Move(selectedFilePath, newFilePath);
+                        listBoxFiles.Items[listBoxFiles.SelectedIndex] = newFilePath;
+                        MessageBox.Show($"Файл успішно перейменований в {newFileName}{extension}");
+                    }
+                    else
+                    {
+                        MessageBox.Show("Файл не знайдений.");
+                    }
+                }
+            }
+        }
+
+        private void btnMoveFile_Click(object sender, EventArgs e)
+        {
+            if (listBoxFiles.SelectedIndex != -1)
+            {
+                string selectedFilePath = listBoxFiles.SelectedItem.ToString();
+
+                using (var folderDialog = new FolderBrowserDialog())
+                {
+                    folderDialog.Description = "Виберіть папку для переміщення файлу";
+
+                    if (folderDialog.ShowDialog() == DialogResult.OK)
+                    {
+                        string destinationFolderPath = folderDialog.SelectedPath; 
+
+                        try
+                        {
+                            string fileName = Path.GetFileName(selectedFilePath);
+                            string destinationFilePath = Path.Combine(destinationFolderPath, fileName); 
+
+                            File.Move(selectedFilePath, destinationFilePath); 
+                            listBoxFiles.Items.Remove(selectedFilePath); 
+                            listBoxFiles.Items.Add(destinationFilePath); 
+
+                            MessageBox.Show("Файл успішно переміщено!");
+                        }
+                        catch (Exception ex)
+                        {
+                            MessageBox.Show($"Помилка переміщення файлу: {ex.Message}");
+                        }
+                    }
+                }
+            }
+        }
+
+        private void btnHelp_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("Щоб завантажити файл з інтернету потрібно ввести URL-адресу в поле для пошуку, вибрати кількість потоків (за необхідності), папку, в яку буде завантажено файл та натиснути \"Завантажити\".\n\nЩоб припинити процес завантаження поруч є кнопка \"Скасувати\".\n\nДля того, щоб видалити файл, необхідно натиснути на необхідний шлях до файлу в таблиці \"Вдалі завантаження\" та натиснути кнопку \"Видалити\".\n\nДля того, щоб перейменувати файл, необхідно натиснути на необхідний шлях до файлу в таблиці \"Вдалі завантаження\", написати в полі під кнопкою нову назву файлу (без розширення) та натиснути кнопку \"Перейменувати\".\n\nДля того, щоб перемістити файл, необхідно натиснути на необхідний шлях до файлу в таблиці \"Вдалі завантаження\", натиснути кнопку \"Перемістити\" та вибрати нову папку.", "Довідка");
+        }
+    }
+}
